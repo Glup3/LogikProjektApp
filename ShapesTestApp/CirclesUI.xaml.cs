179b06b5682fe68adfd90ce8b92f9d2474715cae
Glup3 <glupstar312@gmail.com>
2017-12-05 21:43:41 +0100

    Projektdateien hinzufügen.

---
+++ ShapesTestApp/CirclesUI.xaml.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ShapesTestApp
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class CirclesUI : UserControl
+    {
+        #region private Variablen
+
+        /// <summary>
+        /// Zufallsgenerator
+        /// </summary>
+        Random rng = new Random();
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Die Mengen Operation
+        /// </summary>
+        public string Operation { get; set; }
+
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Standard Konstruktor, erstellt das entsprechende Venn Diagramm
+        /// </summary>
+        /// <param name="farbe1">Farbe für den ersten Kreis</param>
+        /// <param name="farbe2">Farbe für den zweiten Kreis</param>
+        /// <param name="mengenOperation">Mengen Operation fürs Venn Diagramm</param>
+        /// <param name="l1">Menge A</param>
+        /// <param name="l2">Menge B</param>
+        public CirclesUI(Color farbe1, Color farbe2, string mengenOperation, List<int> l1, List<int> l2)
+        {
+            InitializeComponent();
+
+            links.Fill = new SolidColorBrush(farbe1);
+            rechts.Fill = new SolidColorBrush(farbe2);
+
+            GetArea(mengenOperation);
+
+            InsertNumbers(l1, l2);
+
+            MengenOperation(mengenOperation, l1, l2);
+
+            Operation = mengenOperation;
+        }
+
+        #endregion
+
+        #region Hilfsmethoden
+        /// <summary>
+        /// Gibt den Schnittbereich der Kreise aus und färbt in entsprechend
+        /// </summary>
+        /// <param name="fall">Mengen Operation</param>
+        private void GetArea(string fall)
+        {
+            var mode = GeometryCombineMode.Union;
+            Color farbe = Colors.Transparent;
+
+            switch (fall)
+            {
+                case "verein":
+                    mode = GeometryCombineMode.Intersect;
+                    farbe = GetCombinedColor();
+                    ((SolidColorBrush)links.Fill).Color = farbe;
+                    ((SolidColorBrush)rechts.Fill).Color = farbe;
+                    break;
+                case "schnitt":
+                    mode = GeometryCombineMode.Intersect;
+                    farbe = GetCombinedColor();
+                    break;
+                case "symetrie":
+                    mode = GeometryCombineMode.Intersect;
+                    farbe = Colors.White;
+                    break;
+                case "ohne":
+                    mode = GeometryCombineMode.Xor;
+                    break;
+            }
+
+            mitte.Data = Geometry.Combine(kreis1, kreis2, mode, null);
+            mitte.Fill = new SolidColorBrush(farbe);
+        }
+
+        /// <summary>
+        /// Kombiniert zwei Farben
+        /// </summary>
+        /// <returns>Kombination der zwei Farben</returns>
+        private Color GetCombinedColor()
+        {
+            var f1 = ((SolidColorBrush)links.Fill).Color;
+            var f2 = ((SolidColorBrush)rechts.Fill).Color;
+
+            if ((f1 == Colors.Red && f2 == Colors.Blue) || (f2 == Colors.Red && f1 == Colors.Blue)) // violett
+            {
+                return Colors.DarkViolet;
+            }
+
+            if ((f1 == Colors.Red && f2 == Colors.Yellow) || (f2 == Colors.Red && f1 == Colors.Yellow)) // orange
+            {
+                return Colors.Orange;
+            }
+
+            if ((f1 == Colors.Blue && f2 == Colors.Yellow) || (f2 == Colors.Blue && f1 == Colors.Yellow)) // green
+            {
+                return Colors.Green;
+            }
+
+            if ((f1 == f2))
+            {
+                return f1;
+            }
+
+            return Colors.Black;
+        }
+
+        /// <summary>
+        /// Fügt Zahlen zufällig in die TextBoxen ein
+        /// </summary>
+        /// <param name="l1">Menge A</param>
+        /// <param name="l2">Menge B</param>
+        private void InsertNumbers(List<int> l1, List<int> l2)
+        {
+            List<TextBlock> boxenLeft = new List<TextBlock>();
+            List<TextBlock> boxenRight = new List<TextBlock>();
+
+            foreach (var tb in Canva_Left.Children)
+            {
+                boxenLeft.Add((TextBlock)tb);
+            }
+
+            foreach (var tb in Canva_Right.Children)
+            {
+                boxenRight.Add((TextBlock)tb);
+            }
+
+            foreach (int i in l1)
+            {
+                while (true)
+                {
+                    int z = rng.Next(boxenLeft.Count);
+                    if ((boxenLeft.ElementAt((z))).Text == "")
+                    {
+                        boxenLeft.ElementAt(z).Text = i.ToString();
+                        break;
+                    }
+                }
+            }
+
+            foreach (int i in l2)
+            {
+                while (true)
+                {
+                    int z = rng.Next(boxenRight.Count);
+                    if ((boxenRight.ElementAt((z))).Text == "")
+                    {
+                        boxenRight.ElementAt(z).Text = i.ToString();
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Führt die jeweilige Mengenoperation durch und erstellt dadurch das Diagramm
+        /// </summary>
+        /// <param name="m">Mengenoperation</param>
+        /// <param name="l1">Menge A</param>
+        /// <param name="l2">Menge B</param>
+        private void MengenOperation(string m, List<int> l1, List<int> l2)
+        {
+            // Liste mit NUR Doppelten
+            List<int> l4 = new List<int>();
+
+            foreach (int a in l1)
+            {
+                if (l2.Contains(a))
+                {
+                    l4.Add(a);
+                }
+            }
+
+            #region TextBoxen
+            // liste mit TextBoxen links
+            List<TextBlock> tbsLeft = new List<TextBlock>();
+            foreach (UIElement elem in Canva_Left.Children)
+            {
+                tbsLeft.Add((TextBlock)elem);
+            }
+
+            // liste mit TextBoxen Mitte
+            List<TextBlock> tbsCenter = new List<TextBlock>();
+            foreach (UIElement elem in Canva_Center.Children)
+            {
+                tbsCenter.Add((TextBlock)elem);
+            }
+
+            // liste mit TextBoxen rechts
+            List<TextBlock> tbsRight = new List<TextBlock>();
+            foreach (UIElement elem in Canva_Right.Children)
+            {
+                tbsRight.Add((TextBlock)elem);
+            }
+
+            // liste mit ALLEN textboxen
+            List<TextBlock> tbs = new List<TextBlock>();
+            tbs.AddRange(tbsLeft);
+            tbs.AddRange(tbsCenter);
+            tbs.AddRange(tbsRight);
+
+            // alle TextBoxen clearen
+            foreach (var tb in tbs)
+            {
+                tb.Text = "";
+            }
+            #endregion
+
+            #region Vereinigungsmenge Outdated
+            /*
+            if (m == "verein")
+            {
+                // beide Listen zusammen ohne doppelte
+                List<int> l3 = new List<int>();
+
+                foreach (int i in l1)
+                {
+                    if (!l3.Contains(i))
+                        l3.Add(i);
+                }
+                foreach (int i in l2)
+                {
+                    if (!l3.Contains(i))
+                        l3.Add(i);
+                }
+
+                // l3-Werte zufällig in alle Boxen
+                foreach (int i in l3)
+                {
+                    while(true)
+                    {
+                        int z = rng.Next(tbs.Count);
+                        if(tbs[z].Text == "")
+                        {
+                            tbs[z].Text = i.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            */
+            #endregion 
+
+            #region Schnittmenge
+            if (m == "schnitt")
+            {
+                foreach (int i in l4)
+                {
+                    while (true)
+                    {
+                        int z = rng.Next(tbsCenter.Count);
+                        if (tbsCenter[z].Text == "")
+                        {
+                            tbsCenter[z].Text = i.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            #region Symetrische Differenz & Vereinigungsmenge
+            if (m == "symetrie" || m == "verein")
+            {
+                // Menge A ohne B
+                List<int> l5 = l1;
+                // Menge B ohne A
+                List<int> l6 = l2;
+
+                // Menge A und B ohne Doppelte
+                foreach (int i in l4)
+                {
+                    if (l5.Contains(i))
+                    {
+                        l5.Remove(i);
+                    }
+                    if (l6.Contains(i))
+                    {
+                        l6.Remove(i);
+                    }
+                }
+
+                foreach (int i in l5)
+                {
+                    while (true)
+                    {
+                        int z = rng.Next(tbsLeft.Count);
+                        if (tbsLeft[z].Text == "")
+                        {
+                            tbsLeft[z].Text = i.ToString();
+                            break;
+                        }
+                    }
+                }
+
+                foreach (int i in l6)
+                {
+                    while (true)
+                    {
+                        int z = rng.Next(tbsRight.Count);
+                        if (tbsRight[z].Text == "")
+                        {
+                            tbsRight[z].Text = i.ToString();
+                            break;
+                        }
+                    }
+                }
+
+                foreach (int i in l4)
+                {
+                    while (true)
+                    {
+                        int z = rng.Next(tbsCenter.Count);
+                        if (tbsCenter[z].Text == "")
+                        {
+                            tbsCenter[z].Text = i.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            #region Differenz
+            if (m == "ohne")
+            {
+                // Menge A ohne B
+                List<int> l7 = l1;
+                foreach (int i in l2)
+                {
+                    if (l7.Contains(i))
+                        l7.Remove(i);
+                }
+
+                foreach (int i in l7)
+                {
+                    while (true)
+                    {
+                        int z = rng.Next(tbsLeft.Count);
+                        if (tbsLeft[z].Text == "")
+                        {
+                            tbsLeft[z].Text = i.ToString();
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            #endregion
+
+        }
+        
+        #endregion
+
+    }
+}
---
+++ ShapesTestApp/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ShapesTestApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Properties
+
+        /// <summary>
+        /// Farbe für den linken Kreis (Menge A)
+        /// </summary>
+        private Color farbeA;
+
+        /// <summary>
+        /// Farbe für den rechten Kreise (Menge B)
+        /// </summary>
+        private Color farbeB;
+
+        /// <summary>
+        /// Das Diagram rechts
+        /// </summary>
+        private CirclesUI elem;
+
+        #endregion
+
+        #region Konstruktor
+
+        /// <summary>
+        /// Standard Konstruktor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            //elem = new CirclesUI(Colors.White, Colors.White, "verein", new List<int>(), new List<int>());
+            //StackPanel_Container.Children.Add(elem);
+            LoadColors();
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Wenn ein RadioButton gedrückt wird, dann ändert sich die Farbe 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var button = (RadioButton)sender;
+            string name = button.Name;
+
+            switch(name)
+            {
+                case "RotA":
+                    farbeA = Colors.Red; break;
+
+                case "BlauA":
+                    farbeA = Colors.Blue; break;
+
+                case "GelbA":
+                    farbeA = Colors.Yellow; break;
+
+                case "RotB":
+                    farbeB = Colors.Red; break;
+
+                case "BlauB":
+                    farbeB = Colors.Blue; break;
+
+                case "GelbB":
+                    farbeB = Colors.Yellow; break;
+            }
+        }
+
+        /// <summary>
+        /// Zahlen Checkboxen werden ausgecheckt. Das Venn Diagramm wird gecleart
+        /// </summary>
+        /// <param name="sender">Der Button "Clear"</param>
+        /// <param name="e"></param>
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            foreach (UIElement elem in Grid_A.Children)
+            {
+                if (elem is CheckBox check)
+                {
+                    check.IsChecked = false;
+                }
+            }
+            foreach (UIElement elem in Grid_B.Children)
+            {
+                if (elem is CheckBox check)
+                {
+                    check.IsChecked = false;
+                }
+            }
+            StackPanel_Container.Children.Remove(elem);
+            elem = null;
+        }
+
+        /// <summary>
+        /// Wenn sich eine Farbe ändert dann soll sich der Hintergrund entsprechend ändern
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Color selectedColor = (Color)(cmbColors.SelectedItem as PropertyInfo).GetValue(null, null);
+            this.Background = new SolidColorBrush(selectedColor);
+        }
+
+        private void CheckBox_Aktualisieren(object sender, RoutedEventArgs e)
+        {
+            if (elem != null)
+                MengenOperation(elem.Operation);
+        }
+
+        #endregion
+
+        #region Hilfsmethoden
+
+        /// <summary>
+        /// Wenn man auf eine Mengen Operation Button drückt dann soll das jeweilige Venn Diagramm kommen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_MengenOperation(object sender, RoutedEventArgs e)
+        {
+            var b = (Button)sender;
+            MengenOperation(b.Name);
+        }
+
+        /// <summary>
+        /// Erstellung des Venn Diagramms
+        /// </summary>
+        /// <param name="mengenOperation">Mengen Operation</param>
+        private void MengenOperation(string mengenOperation)
+        {
+            StackPanel_Container.Children.Remove(elem);
+            var l1 = GetCheckedBoxes("A");
+            var l2 = GetCheckedBoxes("B");
+
+            elem = new CirclesUI(farbeA, farbeB, mengenOperation, l1, l2);
+            StackPanel_Container.Children.Add(elem);
+        }
+
+        /// <summary>
+        /// Man bekommt die Zahlen der Menge A oder B
+        /// </summary>
+        /// <param name="menge">Menge A oder B</param>
+        /// <returns>Liste mit den Zahlen aus Menge</returns>
+        private List<int> GetCheckedBoxes(string menge)
+        {
+            List<int> list = new List<int>();
+            UIElementCollection coll;
+            if (menge == "A")
+            {
+                coll = Grid_A.Children;
+            }
+            else
+            {
+                coll = Grid_B.Children;
+            }
+
+            foreach (UIElement elem in coll)
+            {
+                if (elem is CheckBox check)
+                {
+                    var txt = check.Content.ToString();
+                    if (check.IsChecked == true)
+                        list.Add(int.Parse(txt));
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Lädt die Farben in den ColorPicker und wählt weiß als Default aus
+        /// </summary>
+        private void LoadColors()
+        {
+            cmbColors.ItemsSource = typeof(Colors).GetProperties();
+            cmbColors.SelectedIndex = cmbColors.Items.Count - 4;
+        }
+
+
+
+        #endregion
+        
+    }
+}
